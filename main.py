7238373c27c250cc0f77b01a66471bc415f63dcd
JuanCastillo240301 <114691151+JuanCastillo240301@users.noreply.github.com>
2023-07-04 20:35:02 -0600

    final version

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
+import tkinter as tk
+from tkinter import messagebox
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+# Función para realizar la predicción y mostrar el resultado en una ventana de mensaje
+def perform_prediction():
+    # Obtener los valores ingresados por el usuario
+    user_input = []
+    for entry in entries:
+        value = entry.get()
+        user_input.append(float(value))
+
+    # Preprocesar los datos de entrada del usuario
+    user_input = scaler.transform([user_input])
+
+    # Realizar la predicción del tumor
+    prediction = knn.predict(user_input)[0]
+
+    # Mostrar el resultado de la predicción en una ventana de mensaje
+    if prediction == 1:
+        messagebox.showinfo("Resultado", "El tumor es maligno.")
+    else:
+        messagebox.showinfo("Resultado", "El tumor es benigno.")
+
+# Crear la ventana principal
+window = tk.Tk()
+window.title("Sistema de Pre Diagnóstico del Cáncer de Mama")
+
+# Estilos de la ventana
+window.configure(bg="#F5F5F5")
+window.geometry("400x300")
 
 # Cargar el conjunto de datos desde el archivo CSV descargado
 df = pd.read_csv('C:/Users/oem/Downloads/breast-cancer/data.csv')
@@ -28,29 +60,39 @@ X = imputer.fit_transform(X)
 scaler = StandardScaler()
 X = scaler.fit_transform(X)
 
+# Dividir los datos en conjuntos de entrenamiento y prueba
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
 # Crear un clasificador KNN
 knn = KNeighborsClassifier(n_neighbors=5)
 
-# Entrenar el modelo KNN con todos los datos
-knn.fit(X, y)
+# Entrenar el modelo KNN con los datos de entrenamiento
+knn.fit(X_train, y_train)
 
 # Obtener la lista de nombres de características
 feature_names = df.columns[1:]
 
-# Solicitar al usuario que ingrese las características del tumor
-user_input = []
-for feature in feature_names:
-    value = input(f"Ingrese el valor de {feature}: ")
-    user_input.append(float(value))
+# Crear etiquetas y campos de entrada para las características
+labels = []
+entries = []
+for i, feature in enumerate(feature_names):
+    if i < len(feature_names) // 2:
+        label = tk.Label(window, text=feature + ":", bg="#F5F5F5")
+        entry = tk.Entry(window)
+        label.grid(row=i, column=0, pady=5, padx=10, sticky="w")
+        entry.grid(row=i, column=1, pady=5, padx=10, sticky="e")
+    else:
+        label = tk.Label(window, text=feature + ":", bg="#F5F5F5")
+        entry = tk.Entry(window)
+        label.grid(row=i - len(feature_names) // 2, column=2, pady=5, padx=10, sticky="w")
+        entry.grid(row=i - len(feature_names) // 2, column=3, pady=5, padx=10, sticky="e")
+    labels.append(label)
+    entries.append(entry)
 
-# Preprocesar los datos de entrada del usuario
-user_input = scaler.transform([user_input])
+# Crear botón para realizar la predicción
+predict_button = tk.Button(window, text="Realizar Predicción", command=perform_prediction, bg="#007BFF", fg="white")
+predict_button.grid(row=len(feature_names)//2, column=0, columnspan=4, pady=10)
 
-# Realizar la predicción del tumor
-prediction = knn.predict(user_input)[0]
+# Ejecutar el bucle principal de la ventana
+window.mainloop()
 
-# Mostrar el resultado de la predicción
-if prediction == 1:
-    print("El tumor es maligno.")
-else:
-    print("El tumor es benigno.")
